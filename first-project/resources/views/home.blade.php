7e14ee5c69b9eda0fa9e7efedc12e5ed3f74c044
CarlosDCorrea <carlosdcorrea1999@hotmail.com>
2021-04-30 11:37:31 -0500

    second commit

--- first-project/resources/views/home.blade.php
+++ first-project/resources/views/home.blade.php
@@ -7,15 +7,18 @@
             <img src="/img/universe_powershell.jpg" style="height:120px; width:120px" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+            <h1>{{ $user->username }}</h1>
+            <a href="#">Create a post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-4"><strong>153</strong> Posts</div>
                 <div class="pr-4"><strong>23k</strong> followers</div>
                 <div class="pr-4"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">PythonUniverse.com</div>
-            <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At omnis ratione atque maiores quidem aliquid vero ut, unde placeat fuga.</div>
-            <div><a href="#">www.PythonUniverse.com</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
     <div class="row pt-5">
